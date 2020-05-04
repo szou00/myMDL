812f7d87baad6d4666e8083e2f39ad69b34fa9a2
Sharon Zou <szou00@stuy.edu>
2020-05-04 02:17:57 -0400

    updates

--- makefile
+++ makefile
@@ -9,13 +9,13 @@ run: parser
 parser: lex.yy.c y.tab.c y.tab.h $(OBJECTS)
 	gcc -o mdl $(CFLAGS) lex.yy.c y.tab.c $(OBJECTS) $(LDFLAGS)
 
-lex.yy.c: mdl.l y.tab.h 
+lex.yy.c: mdl.l y.tab.h
 	flex -I mdl.l
 
 y.tab.c: mdl.y symtab.h parser.h
 	bison -d -y mdl.y
 
-y.tab.h: mdl.y 
+y.tab.h: mdl.y
 	bison -d -y mdl.y
 
 symtab.o: symtab.c parser.h matrix.h
--- my_main.c
+++ my_main.c
@@ -57,7 +57,7 @@ void my_main() {
   screen t;
   zbuffer zb;
   color g;
-  double step_3d = 20;
+  double step_3d = 100;
   double theta;
 
   //Lighting values here for easy access
@@ -71,7 +71,7 @@ void my_main() {
   light[LOCATION][1] = 0.75;
   light[LOCATION][2] = 1;
 
-  light[COLOR][RED] = 0;
+  light[COLOR][RED] = 255;
   light[COLOR][GREEN] = 255;
   light[COLOR][BLUE] = 255;
 
@@ -110,6 +110,271 @@ void my_main() {
   for (i=0;i<lastop;i++) {
 
     printf("%d: ",i);
+    switch (op[i].opcode)
+      {
+      case LIGHT:
+        printf("Light: %s at: %6.2f %6.2f %6.2f",
+               op[i].op.light.p->name,
+               op[i].op.light.c[0], op[i].op.light.c[1],
+               op[i].op.light.c[2]);
+        break;
+      case AMBIENT:
+        printf("Ambient: %6.2f %6.2f %6.2f",
+               op[i].op.ambient.c[0],
+               op[i].op.ambient.c[1],
+               op[i].op.ambient.c[2]);
+        break;
+
+      case CONSTANTS:
+        printf("Constants: %s",op[i].op.constants.p->name);
+        break;
+      case SAVE_COORDS:
+        printf("Save Coords: %s",op[i].op.save_coordinate_system.p->name);
+        break;
+      case CAMERA:
+        printf("Camera: eye: %6.2f %6.2f %6.2f\taim: %6.2f %6.2f %6.2f",
+               op[i].op.camera.eye[0], op[i].op.camera.eye[1],
+               op[i].op.camera.eye[2],
+               op[i].op.camera.aim[0], op[i].op.camera.aim[1],
+               op[i].op.camera.aim[2]);
+
+        break;
+
+      // add a box/sphere/torus to a temporary polygon matrix, multiply it by the current top and draw it to the screen
+      // if a constants variable is present, use those for lighting, otherwise, use a default set.
+      // ignore the optional variable at the end of the command.
+      case SPHERE:
+        printf("Sphere: %6.2f %6.2f %6.2f r=%6.2f",
+               op[i].op.sphere.d[0],op[i].op.sphere.d[1],
+               op[i].op.sphere.d[2],
+               op[i].op.sphere.r);
+        add_sphere(tmp, op[i].op.sphere.d[0],op[i].op.sphere.d[1],op[i].op.sphere.d[2],op[i].op.sphere.r,step_3d);
+        matrix_mult(peek(systems),tmp);
+        if (op[i].op.sphere.constants != NULL)
+          {
+            printf("\tconstants: %s",op[i].op.sphere.constants->name);
+            reflect = op[i].op.sphere.constants->s.c;
+            g.red = reflect->red;
+            g.green = reflect->green;
+            g.blue = reflect->blue;
+            draw_polygons(tmp,t,zb,view,light,g,reflect);
+          }
+        else {
+          reflect = &white;
+          draw_polygons(tmp,t,zb,view,light,g,reflect);
+        }
+        if (op[i].op.sphere.cs != NULL)
+          {
+            printf("\tcs: %s",op[i].op.sphere.cs->name);
+          }
+        tmp->lastcol = 0;
+        break;
+      case TORUS:
+        printf("Torus: %6.2f %6.2f %6.2f r0=%6.2f r1=%6.2f",
+               op[i].op.torus.d[0],op[i].op.torus.d[1],
+               op[i].op.torus.d[2],
+               op[i].op.torus.r0,op[i].op.torus.r1);
+
+        add_torus(tmp, op[i].op.torus.d[0], op[i].op.torus.d[1], op[i].op.torus.d[2],
+                        op[i].op.torus.r0,op[i].op.torus.r1,step_3d);
+        matrix_mult(peek(systems),tmp);
+        if (op[i].op.torus.constants != NULL)
+          {
+            printf("\tconstants: %s",op[i].op.torus.constants->name);
+            reflect = op[i].op.torus.constants->s.c;
+            g.red = reflect->red;
+            g.green = reflect->green;
+            g.blue = reflect->blue;
+            draw_polygons(tmp,t,zb,view,light,g,reflect);
+          }
+        else {
+          reflect = &white;
+          draw_polygons(tmp,t,zb,view,light,g,reflect);
+        }
+        if (op[i].op.torus.cs != NULL)
+          {
+            printf("\tcs: %s",op[i].op.torus.cs->name);
+          }
+        tmp->lastcol = 0;
+        break;
+      case BOX:
+        printf("Box: d0: %6.2f %6.2f %6.2f d1: %6.2f %6.2f %6.2f",
+               op[i].op.box.d0[0],op[i].op.box.d0[1],
+               op[i].op.box.d0[2],
+               op[i].op.box.d1[0],op[i].op.box.d1[1],
+               op[i].op.box.d1[2]);
+        add_box(tmp, op[i].op.box.d0[0], op[i].op.box.d0[1], op[i].op.box.d0[2],
+               op[i].op.box.d1[0], op[i].op.box.d1[1], op[i].op.box.d1[2]);
+        matrix_mult(peek(systems),tmp);
+        if (op[i].op.box.constants != NULL)
+          {
+            printf("\tconstants: %s",op[i].op.box.constants->name);
+            reflect = op[i].op.box.constants->s.c;
+            g.red = reflect->red;
+            g.green = reflect->green;
+            g.blue = reflect->blue;
+            draw_polygons(tmp,t,zb,view,light,g,reflect);
+          }
+        else {
+          reflect = &white;
+          draw_polygons(tmp,t,zb,view,light,g,reflect);
+        }
+        if (op[i].op.box.cs != NULL)
+          {
+            printf("\tcs: %s",op[i].op.box.cs->name);
+          }
+        tmp->lastcol = 0;
+        break;
+
+      // add a line to a temporary edge matrix, multiply it by the current top and draw it to the screen
+      // do not try to use the optional arguments for this command
+      case LINE:
+        printf("Line: from: %6.2f %6.2f %6.2f to: %6.2f %6.2f %6.2f",
+               op[i].op.line.p0[0],op[i].op.line.p0[1],
+               op[i].op.line.p0[1],
+               op[i].op.line.p1[0],op[i].op.line.p1[1],
+               op[i].op.line.p1[1]);
+        if (op[i].op.line.constants != NULL)
+          {
+            printf("\n\tConstants: %s",op[i].op.line.constants->name);
+          }
+        if (op[i].op.line.cs0 != NULL)
+          {
+            printf("\n\tCS0: %s",op[i].op.line.cs0->name);
+          }
+        if (op[i].op.line.cs1 != NULL)
+          {
+            printf("\n\tCS1: %s",op[i].op.line.cs1->name);
+          }
+
+        add_edge(tmp, op[i].op.line.p0[0], op[i].op.line.p0[1], op[i].op.line.p0[2],
+                      op[i].op.line.p1[0], op[i].op.line.p1[1], op[i].op.line.p1[2]);
+        matrix_mult(peek(systems),tmp);
+        draw_lines(tmp,t,zb,g);
+        tmp->lastcol = 0;
+        break;
+      //
+      //
+      case MESH:
+        printf("Mesh: filename: %s",op[i].op.mesh.name);
+        if (op[i].op.mesh.constants != NULL)
+          {
+            printf("\tconstants: %s",op[i].op.mesh.constants->name);
+          }
+        break;
+      case SET:
+        printf("Set: %s %6.2f",
+               op[i].op.set.p->name,
+               op[i].op.set.p->s.value);
+        break;
+
+      // create a translation/rotation/scale matrix and multiply the current top by it
+      // do not try to use the optional arguments for these commands
+      case MOVE:
+        printf("Move: %6.2f %6.2f %6.2f",
+               op[i].op.move.d[0],op[i].op.move.d[1],
+               op[i].op.move.d[2]);
+        if (op[i].op.move.p != NULL)
+          {
+            printf("\tknob: %s",op[i].op.move.p->name);
+          }
+        tmp = make_translate(op[i].op.move.d[0],op[i].op.move.d[1],op[i].op.move.d[2]);
+        matrix_mult(peek(systems), tmp);
+        copy_matrix(tmp, peek(systems));
+        tmp->lastcol = 0;
+        break;
+      case SCALE:
+        printf("Scale: %6.2f %6.2f %6.2f",
+               op[i].op.scale.d[0],op[i].op.scale.d[1],
+               op[i].op.scale.d[2]);
+        if (op[i].op.scale.p != NULL)
+          {
+            printf("\tknob: %s",op[i].op.scale.p->name);
+          }
+        tmp = make_scale(op[i].op.scale.d[0],op[i].op.scale.d[1],op[i].op.scale.d[2]);
+        matrix_mult(peek(systems), tmp);
+        copy_matrix(tmp, peek(systems));
+        tmp->lastcol = 0;
+        break;
+      case ROTATE:
+        printf("Rotate: axis: %6.2f degrees: %6.2f",
+               op[i].op.rotate.axis,
+               op[i].op.rotate.degrees);
+        if (op[i].op.rotate.p != NULL)
+          {
+            printf("\tknob: %s",op[i].op.rotate.p->name);
+          }
+        theta = op[i].op.rotate.degrees * (M_PI / 180);
+        if (op[i].op.rotate.axis == 0)
+          tmp = make_rotX( theta );
+        else if (op[i].op.rotate.axis == 1)
+          tmp = make_rotY( theta );
+        else
+          tmp = make_rotZ( theta );
+
+        matrix_mult(peek(systems), tmp);
+        copy_matrix(tmp, peek(systems));
+        tmp->lastcol = 0;
+        break;
+      //
+      //
+      case BASENAME:
+        printf("Basename: %s",op[i].op.basename.p->name);
+        break;
+      case SAVE_KNOBS:
+        printf("Save knobs: %s",op[i].op.save_knobs.p->name);
+        break;
+      case TWEEN:
+        printf("Tween: %4.0f %4.0f, %s %s",
+               op[i].op.tween.start_frame,
+               op[i].op.tween.end_frame,
+               op[i].op.tween.knob_list0->name,
+               op[i].op.tween.knob_list1->name);
+        break;
+      case FRAMES:
+        printf("Num frames: %4.0f",op[i].op.frames.num_frames);
+        break;
+      case VARY:
+        printf("Vary: %4.0f %4.0f, %4.0f %4.0f",
+               op[i].op.vary.start_frame,
+               op[i].op.vary.end_frame,
+               op[i].op.vary.start_val,
+               op[i].op.vary.end_val);
+        break;
+
+      // push a copy of the current top of the origins stack onto the origins stack (a full copy, not just a reference to the current top)
+      case PUSH:
+        printf("Push");
+        push(systems);
+        break;
+      // removes the top of the origins stack (nothing needs to be done with this data)
+      case POP:
+        printf("Pop");
+        pop(systems);
+        break;
+      case GENERATE_RAYFILES:
+        printf("Generate Ray Files");
+        break;
+      // save the screen to the provided file name
+      case SAVE:
+        printf("Save: %s",op[i].op.save.p->name);
+        save_extension(t,op[i].op.save.p->name);
+        break;
+      case SHADING:
+        printf("Shading: %s",op[i].op.shading.p->name);
+        break;
+      case SETKNOBS:
+        printf("Setknobs: %f",op[i].op.setknobs.value);
+        break;
+      case FOCAL:
+        printf("Focal: %f",op[i].op.focal.value);
+        break;
+      // show the image
+      case DISPLAY:
+        printf("Display");
+        display(t);
+        break;
+      }
 
     printf("\n");
   }
